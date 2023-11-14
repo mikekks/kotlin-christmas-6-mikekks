b48cecd66ff3fb27ea968c998bd4fc3796a0425b
mikekks <thdalsrb79@naver.com>
2023-11-14 16:18:32 +0900

    [Feat] GuildMessage 상수화

---
+++ src/main/java/christmas/global/GuildMessage.java
@@ -0,0 +1,28 @@
+package christmas.global;
+
+public class GuildMessage {
+    public static final String REQUEST_VISIT_DATE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n"
+            + "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+
+    public static final String REQUEST_MENU_COUNT = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    public static final String PRINT_EVENT_BENEFITS = "12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+
+    public static final String PRINT_ORDER_MENUS = "<주문 메뉴>";
+    public static final String PRINT_BEFORE_ORDER_PRICE = "<할인 전 총주문 금액>";
+    public static final String PRINT_FREE_GIFT_MENU = "<증정 메뉴>";
+    public static final String PRINT_BENEFIT_DETAILS = "<혜택 내역>";
+    public static final String PRINT_TOTAL_BENEFIT_DETAIL = "<총혜택 금액>";
+    public static final String PRINT_AFTER_ORDER_PRICE = "<할인 후 예상 결제 금액>";
+    public static final String PRINT_EVENT_BADGE = "<12월 이벤트 배지>";
+
+
+
+    public static final String BLANK = " ";
+    public static final String COUNT = "개";
+    public static final String WON = "원";
+    public static final String NONE = "없음";
+
+
+    public static final String NEXT_LINE = "\n";
+
+}
