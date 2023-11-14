87c69df5c682e6a5ced8a85c8642adb7aec6123a
mikekks <thdalsrb79@naver.com>
2023-11-14 16:16:35 +0900

    [Feat] ErrorMessage 상수화

---
+++ src/main/java/christmas/global/ErrorMessage.java
@@ -0,0 +1,8 @@
+package christmas.global;
+
+public class ErrorMessage {
+
+    public static final String INVALID_DATE_FORMAT = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    public static final String INVALID_ORDER_FORMAT = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+
+}
